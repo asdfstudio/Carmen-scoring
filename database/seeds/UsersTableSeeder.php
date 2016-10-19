3e5ce4d90d8854a31ac43779eb54f97c5b340944
Jonathan Kelp <jkelp@804studios.com>
2016-10-19 10:01:13 -0400

    Add carmen user to seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,7 +14,8 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             ['email' => 'ab@804studios.com', 'password' => bcrypt('test'), 'person_id' => 1, 'organization_id' => NULL, 'is_admin' => FALSE],
 						['email' => 'bc@804studios.com', 'password' => bcrypt('test'), 'person_id' => NULL, 'organization_id' => 1, 'is_admin' => FALSE],
-						['email' => 'jkelp@804studios.com', 'password' => bcrypt('test'), 'person_id' => NULL, 'organization_id' => NULL, 'is_admin' => TRUE]
+						['email' => 'jkelp@804studios.com', 'password' => bcrypt('test'), 'person_id' => NULL, 'organization_id' => NULL, 'is_admin' => TRUE],
+            ['email' => 'carmenshowchoir@gmail.com', 'password' => bcrypt('test'), 'person_id' => NULL, 'organization_id' => NULL, 'is_admin' => TRUE]
         ]);
     }
 }
