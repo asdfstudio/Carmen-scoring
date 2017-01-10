a2243a0d4988465ca8d2859c0257dd6d7280a03f
Jonathan Kelp <jkelp@804studios.com>
2017-01-10 14:16:45 -0500

    Public results

--- resources/views/results/division/show.blade.php
+++ resources/views/results/division/show.blade.php
@@ -15,38 +15,37 @@
 		</div>
 
 		@foreach($division->standings as $standing)
-			<div class="standing-container">
 
-				<div class="content-subheader caption {{ $standing->caption_slug }}">
-					@if($standing->caption_id == NULL)
-						<h3>Overall Standings</h3>
-					@else
-						<h3>{{ $standing->caption->name }} Standings</h3>
-					@endif
+			@if($standing)
+				<?php
+				if($standing->caption_id == NULL)
+				{
+					$limit = $division->overall_award_count;
+				}
+				else
+				{
+					$column_name = $standing->caption->slug().'_award_count';
+					$limit = $division->{$column_name};
+				}
+
+				$standing->choirs = $standing->choirs->take($limit)->reverse();
+				?>
+			@endif
+
+			@if($standing->choirs->count() > 0)
+				<div class="standing-container">
+					<div class="content-subheader caption {{ $standing->caption_slug }}">
+						@if($standing->caption_id == NULL)
+							<h3>Overall Standings</h3>
+						@else
+							<h3>{{ $standing->caption->name }} Standings</h3>
+						@endif
+					</div>
+
+					@include('standing.public_list', ['standing' => $standing])
+
 				</div>
-
-				@if($standing == false)
-				  <p>There are no final standings yet.</p>
-				@endif
-
-				@if($standing)
-		      <?php
-		      if($standing->caption_id == NULL)
-		      {
-		        $limit = $division->overall_award_count;
-		      }
-		      else
-		      {
-		        $column_name = $standing->caption->slug().'_award_count';
-		        $limit = $division->{$column_name};
-		      }
-
-		      $standing->choirs = $standing->choirs->take($limit)->reverse();
-		      ?>
-				  @include('standing.public_list', ['standing' => $standing])
-
-				@endif
-			</div>
+			@endif
 		@endforeach
 
 @endsection
