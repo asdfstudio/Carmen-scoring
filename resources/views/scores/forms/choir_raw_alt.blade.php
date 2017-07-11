a7ef7dd338d96cad6af44f37230267934749d06b
Jonathan Kelp <jkelp@804studios.com>
2017-07-11 16:28:00 -0400

    Hide .5 scoring options for workshops

--- resources/views/scores/forms/choir_raw_alt.blade.php
+++ resources/views/scores/forms/choir_raw_alt.blade.php
@@ -34,7 +34,9 @@
 
           @include('scores.forms.number_selector', ['criterion' => $criterion,'score' => $score, 'start' => 1, 'end' => 10, 'interval' => 1])
 
-          @include('scores.forms.number_selector', ['criterion' => $criterion,'score' => $score, 'start' => 0.5, 'end' => 9.5, 'interval' => 1, 'class' => 'half'])
+          @if(!env('IS_WORKSHOP_ENABLED'))
+            @include('scores.forms.number_selector', ['criterion' => $criterion,'score' => $score, 'start' => 0.5, 'end' => 9.5, 'interval' => 1, 'class' => 'half'])
+          @endif
 
         </div>
 
