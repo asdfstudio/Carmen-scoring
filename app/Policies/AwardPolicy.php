7d3c0b36f43abe345c03fc1e290e7cd93098eaa9
Reed Hewitt <reed@reedhewitt.com>
2019-11-12 11:57:51 -0600

    Allow organizers to edit awards after division is marked complete.

--- app/Policies/AwardPolicy.php
+++ app/Policies/AwardPolicy.php
@@ -69,7 +69,7 @@ class AwardPolicy extends BasePolicy
 
     public function manage(User $user, $award, Division $division)
 		{
-        if($this->isOrgAdmin AND $this->orgId === $award->organization_id AND $division->status_slug == 'active')
+        if($this->isOrgAdmin AND $this->orgId === $award->organization_id)
         {
           return true;
         }
