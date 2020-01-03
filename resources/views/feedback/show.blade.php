3e6af9fc2efe9f963c699eb46a1b60c9e2bfb3d2
Reed Hewitt <reed@reedhewitt.com>
2020-01-02 20:33:46 -0600

    Only show judges that have a comment or audio on feedback page.

--- resources/views/feedback/show.blade.php
+++ resources/views/feedback/show.blade.php
@@ -43,44 +43,47 @@
                 $judge_comments = $round_comments->where('judge_id', $judge->id);
                 $judge_recordings = $round_recordings->where('judge_id', $judge->id);
                 $judges = collect();
-                $comments_were_empty = true;
+                $comments_not_empty = false;
+                @foreach($judge_comments as $comment)
+                  @if(!empty($comment->comments))
+                    @php
+                      $comments_not_empty = true;
+                    @endphp
+                  @endif
+                @endforeach
               @endphp
-              <li class="list-group-item unpadded">
-                <div class="header">
-                  {{ $judge->full_name }}
-                </div>
-                <div class="body">
-                  <div>
-                  @if($judge_comments->count())
-                    @foreach($judge_comments as $comment)
-                      @if(!empty($comment->comments))
-                        {!! nl2br($comment->comments) !!}
-                        @php
-                          $comments_were_empty = false
-                        @endphp
-                      @endif
-                    @endforeach
-                  @endif
-                  @if($comments_were_empty)
-                    <i class="text-muted">No typed comments were entered by this judge</i>
-                  @endif
+              @if($comments_not_empty || ($competition->organization->is_premium == 1 && $judge_recordings->count()))
+                <li class="list-group-item unpadded">
+                  <div class="header">
+                    {{ $judge->full_name }}
                   </div>
-                  @if($competition->organization->is_premium == 1 && $judge_recordings->count())
-                  <div class="record-row">
-                    <ol>
-                    @foreach($judge_recordings as $recording)
-                      <li class="record-item">
-                        <div class="record-item-audio">
-                          <audio controls> <source src="{{$recording->url}}"> </audio>
-                          <span> {{$recording->created_at}} (UTC)</span>
-                        </div>
-                      </li>
-                    @endforeach
-                    </ol>
-                  </div>   
-                  @endif            
-                </div>
-              </li>
+                  <div class="body">
+                    <div>
+                    @if($comments_not_empty)
+                      @foreach($judge_comments as $comment)
+                        {!! nl2br($comment->comments) !!}
+                      @endforeach
+                    @else
+                      <i class="text-muted">No typed comments were entered by this judge</i>
+                    @endif
+                    </div>
+                    @if($competition->organization->is_premium == 1 && $judge_recordings->count())
+                    <div class="record-row">
+                      <ol>
+                      @foreach($judge_recordings as $recording)
+                        <li class="record-item">
+                          <div class="record-item-audio">
+                            <audio controls> <source src="{{$recording->url}}"> </audio>
+                            <span> {{$recording->created_at}} (UTC)</span>
+                          </div>
+                        </li>
+                      @endforeach
+                      </ol>
+                    </div>   
+                    @endif            
+                  </div>
+                </li>
+              @endif
             @endforeach
           </ul>
         @endif
