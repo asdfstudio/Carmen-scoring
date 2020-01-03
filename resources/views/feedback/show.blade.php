ec8fcc6e75765bd05b44a3fc56bbd310ae09b4d7
Reed Hewitt <reed@reedhewitt.com>
2020-01-02 20:21:32 -0600

    Restructure comments and recordings so they don't depend on each other in feeback view.

--- resources/views/feedback/show.blade.php
+++ resources/views/feedback/show.blade.php
@@ -25,25 +25,43 @@
         @endif
 
         @if($round->is_completed)
+          @php
+            $round_comments = $comments->where('subject_id', $round->id);
+            $round_recordings = $recordings->where('round_id', $round->id);
+            $judges = collect();
+            foreach($round_comments as $comment){
+               $judges->push($comment->judge);
+            }
+            foreach($round_recordings as $recording){
+               $judges->push($recording->judge);
+            }
+            $judges = $judges->unique();
+          @endphp
           <ul class="list-group">
-            @foreach($comments->where('subject_id', $round->id)->where('subject_type', 'App\Round') as $comment)
+            @foreach($judges as $judge)
+              @php
+                $judge_comments = $round_comments->where('judge_id', $judge->id);
+                $judge_recordings = $round_recordings->where('judge_id', $judge->id);
+                $judges = collect();
+              @endphp
               <li class="list-group-item unpadded">
                 <div class="header">
-                {{$comment->choir_id}}
-                  {{ $comment->judge->full_name }}
+                  {{ $judge->full_name }}
                 </div>
                 <div class="body">
                   <div>
-                    @if($comment->comments)
-                      {!! nl2br($comment->comments) !!}
+                    @if($judge_comments->count())
+                      @foreach($judge_comments as $comment)
+                        {!! nl2br($comment->comments) !!}
+                      @endforeach
                     @else
                       <i class="text-muted">No typed comments were entered by this judge</i>
                     @endif
                   </div>
-                  @if($competition->organization->is_premium == 1)
+                  @if($competition->organization->is_premium == 1 && $judge_recordings->count())
                   <div class="record-row">
                     <ol>
-                    @foreach($recordings->where('round_id', $round->id)->where('choir_id', $comment->choir_id)->where('judge_id', $comment->judge_id) as $recording)
+                    @foreach($judge_recordings as $recording)
                       <li class="record-item">
                         <div class="record-item-audio">
                           <audio controls> <source src="{{$recording->url}}"> </audio>
