925f82c571b8d48b32fd152ed2c2fb558510fd83
Reed Hewitt <reed@reedhewitt.com>
2020-01-17 17:34:40 -0600

    Add scoring method note to results pages.

--- resources/views/results/division_round/show.blade.php
+++ resources/views/results/division_round/show.blade.php
@@ -14,7 +14,9 @@
 			<p>Click on the name of a <strong>choir</strong> or <strong>judge</strong> to view their score details.</p>
 		</div>
 	@endif
-
+  
+  <p class="scoring-method-note">This division uses the <strong>{{ $division->scoringMethod->name }}</strong> method of scoring. <a href="https://carmenscoring.com/scoring-methods" target="blank">View scoring method explanations</a>.</p>
+  
   {{-- Raw Scoring, 50/50 --}}
   @if ($division->scoring_method_id === 1 && $division->caption_weighting_id === 2)
     <ul class="list-group horizontal">
