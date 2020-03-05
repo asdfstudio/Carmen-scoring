af5d565bd059a88feb46d75f9d2b9841d439773e
Reed Hewitt <reed@reedhewitt.com>
2020-03-05 21:33:05 +0000

    Revamp audio recorder.

--- public/js/recording.js
+++ public/js/recording.js
@@ -1,150 +1,277 @@
-// Customize the behaviour of the form that creates or edits a user or person.
+// Array to hold all audio recorder instances that may be on the page.
+window.audioRecorers = []
 
-// webkitURL is deprecated but nevertheless
-URL = window.URL || window.webkitURL
+// The audio recorder class that will be initialized for each audio recorder widget on the page.
+function AudioRecorder (element) {
 
-var gumStream					// stream from getUserMedia()
-var rec					// Recorder.js object
-var audioRecorder
-var recorder
-var recordButton = document.getElementById('recordButton')
-var stopButton = document.getElementById('stopButton')
-var recordData = []
-var recordingsInProgress = 0
-var uploadsInProgress = 0
+  /*================================================================================
+      Properties
+    ================================================================================*/
 
-// eslint-disable-next-line no-unused-vars
-function deleteRecording (id) {
-  if (confirm('Are you sure you want to delete the recording?') == true) {
-    $.ajaxSetup({
-      headers: {
-        'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-      }
-    })
-    $.ajax({
-      url: '/organizer/recording/delete/' + id,
-      method: 'DELETE',
-      success: function (result) {
-        location.reload()
-      }
-    })
+  this.widget = $(element)
+  this.otherWidgets = $('.audio-recorder').not(this.widget)
+  this.button = $(this.widget).find('.ar-control button')
+  this.existingLabel = $(this.widget).find('.ar-existing')
+  this.progressText = $(this.widget).find('.ar-progress-text')
+  this.recordingTime = 0
+  this.recordingTimer = null
+  this.progressMeterBar = $(this.widget).find('.ar-meter-bar')
+  this.micRecorder = new MicRecorder({bitRate: 128})
+  this.choirId = parseInt(this.widget.data('choir')) || 0
+  this.roundId = parseInt(this.widget.data('round')) || 0
+  this.divisionId = parseInt(this.widget.data('division')) || 0
+  this.existingRecordingCount = parseInt(this.widget.data('count')) || 0
+  this.recordingInProgress = false
+  this.uploadInProgress = false
+  this.uploadProgressCounter = 0
+  this.uploadSuccessDenouement = false
+
+
+  /*================================================================================
+      Event Listeners
+    ================================================================================*/
+
+  this.button.click((e) => {
+    e.preventDefault()
+    if(!this.widget.hasClass('disabled')){
+      this.startRecording();
+    }
+  });
+
+  $(window).on('beforeunload', () => {
+    if (this.recordingInProgress) {
+      return 'Recording in progress. Navigating away from the page will lose your recording. Are you sure you want to continue?'
+    }
+    if (this.uploadInProgress) {
+      return 'Upload in progress. Navigating away from the page will lose your file. Are you sure you want to continue?'
+    }
+  })
+
+
+  /*================================================================================
+      Methods
+    ================================================================================*/
+
+  this.startRecording = () => {
+    if (!this.recordingInProgress) {
+      // Start recording.
+      this.micRecorder.start()
+        .then(() => {
+          console.log('Recording started.')
+
+          this.recordingInProgress = true
+          this.startRecordingTime()
+
+          this.otherWidgets.addClass('disabled')
+          this.widget.addClass('recording')
+        })
+        .catch((error) => {
+          console.error(error)
+
+          if (error.name === 'NotFoundError') {
+            alert('Please connect or enable your microphone.')
+          } else if (error.name === 'TypeError') {
+            alert('Your browser does not support recording.')
+          } else {
+            alert('There was an error when trying to record.')
+          }
+          return false
+        })
+    } else {
+      // Stop recording.
+      this.micRecorder.stop()
+        .getMp3()
+        .then(([buffer, blob]) => {
+          console.log('Recording stopped.')
+
+          this.recordingInProgress = false
+          this.clearRecordingTime()
+
+          this.widget.removeClass('recording').addClass('disabled')
+
+          this.uploadRecording(blob)
+        })
+        .catch((error) => {
+          console.error(error)
+
+          this.warnRecordingSaveError();
+        })
+    }
   }
-}
 
-function startRecording (choirId, roundId, divisionId) {
-  console.log('recordButton clicked')
-  var button = $('#recordButton-' + choirId)
-  var isRecording = parseInt(button.attr('data-recording')) || 0
-  console.log('isRecording', isRecording)
-  var count = parseInt(button.attr('data-count')) || 0
-  console.log('count', count)
-  recordData.choirId = choirId
-  recordData.roundId = roundId
-  recordData.divisionId = divisionId
-  if (isRecording === 0) {
-    recorder = new MicRecorder({
-      bitRate: 128
-    })
-    /*
-        Disable the record button until we get a success or fail from getUserMedia()
-    */
-    recorder
-      .start()
-      .then(() => {
-        recordingsInProgress++
-        $('.rbutton').addClass('cancel')
-        button.removeClass('cancel')
-        button.text('Stop Recording')
-        button.attr('data-recording', 1)
-        button.attr('data-count', count + 1)
-        console.log('Recording started')
-      // something else
-      })
-      .catch(e => {
-        console.error(e)
-        if (e.name === 'NotFoundError') {
-          alert('Please plugin your microphone')
-        } else if (e.name === 'TypeError') {
-          alert('Your browser does not support recording')
-        } else {
-          alert('Something went wrong')
+  this.uploadRecording = (blob) => {
+    var formData = new FormData()
+    formData.append('division_id', this.divisionId)
+    formData.append('round_id', this.roundId)
+    formData.append('choir_id', this.choirId)
+    formData.append('file', blob)
+
+    $.ajax(
+      {
+        url: '/judge/recording/save',
+        method: 'POST',
+        data: formData,
+        cache: false,
+        contentType: false, // must, tell jQuery not to process the data
+        processData: false,
+        xhr: () => {
+          var xhr = new window.XMLHttpRequest();
+
+          if(xhr.upload){
+            xhr.upload.addEventListener('progress', this.uploadProgressHandler, false);
+          }
+
+          return xhr
+        },
+        beforeSend: (jqXHR, settings) => {
+          jqXHR.setRequestHeader('X-CSRF-TOKEN', $('meta[name="_token"]').attr('content'))
+
+          this.uploadInProgress = true
+          this.updateProgressText()
+
+          this.widget.addClass('uploading unknown')
+
+          console.log('Uploading...')
         }
-        return false
-      })
-  } else {
-    // stop recording
-    console.log('stopping...')
-    recorder.stop().getMp3().then(([buffer, blob]) => {
-      recordingsInProgress--
-      const file = new File(buffer, 'music.mp3', {
-        type: blob.type,
-        lastModified: Date.now()
-      })
-      $('.rbutton').removeClass('cancel')
-      button.text('Start Recording (' + count + ')')
-      button.attr('data-recording', 0)
-      uploadRecording(blob)
-    }).catch((e) => {
-      console.error(e)
-      warnRecordingSaveError();
-    })
-  }
-}
-
-function uploadRecording (blob) {
-  var formData = new FormData()
-  formData.append('division_id', recordData.divisionId)
-  formData.append('round_id', recordData.roundId)
-  formData.append('file', blob)
-  formData.append('choir_id', recordData.choirId)
-  $.ajaxSetup({
-    headers: {
-      'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-    }
-  })
-  $('#sliderId-' + recordData.choirId).show()
-  uploadsInProgress++
-  $.ajax({
-    url: '/judge/recording/save',
-    method: 'POST',
-    data: formData,
-    cache: false,
-    contentType: false, // must, tell jQuery not to process the data
-    processData: false,
-    success: function (result) {
-      console.log(result)
-      $('#sliderId-' + recordData.choirId).hide()
-      if(typeof result.url === 'undefined'){
-        warnRecordingSaveError()
       }
-    },
-    error: function() {
-      warnRecordingSaveError()
-    },
-    complete: function() {
-      uploadsInProgress--
+    ).done(
+      (result, textStatus, jqXHR) => {
+        if(typeof result.url === 'undefined'){
+          console.log('Upload resulted in server-side error.')
+
+          this.warnUploadRecordingError()
+        } else {
+          console.log('Upload completed successfully.')
+
+          this.existingRecordingCount++
+          this.uploadSuccessDenouement = true
+
+          this.widget.data('count', this.existingRecordingCount)
+          this.progressMeterBar.css('width', '100%')
+          this.widget.removeClass('unknown')
+          this.existingLabel.text(this.existingRecordingCount + ' ' + (this.existingRecordingCount == 1 ? 'Recording' : 'Recordings') + ' on File')
+        }
+
+        console.log('Result:', result)
+      }
+    ).fail(
+      (jqXHR, textStatus, errorThrown) => {
+        console.log('Upload resulted in AJAX error.')
+        console.log('jqXHR:', jqXHR)
+
+        this.warnUploadRecordingError()
+      }
+    ).always(
+      () => {
+        this.uploadInProgress = false
+        this.uploadProgressCounter = 0
+        this.updateProgressText()
+
+        if(this.uploadSuccessDenouement) {
+          // After a successful upload, allow the widget to linger in the upload state for a moment
+          // so that the user has time to understand that it is complete.
+          setTimeout(() => {
+            this.uploadSuccessDenouement = false
+            this.updateProgressText()
+            this.progressMeterBar.css('width', '')
+            this.widget.removeClass('disabled uploading unknown')
+            this.otherWidgets.removeClass('disabled')
+          }, 2000)
+        } else {
+          // If this wasn't a successful upload, just reset the widget appearance.
+          // The user already got a warning message.
+          this.widget.removeClass('disabled uploading unknown')
+          this.otherWidgets.removeClass('disabled')
+        }
+      }
+    )
+  }
+
+  this.uploadProgressHandler = (progress) => {
+    var percent = 0
+
+    if(progress.lengthComputable){
+      percent = Math.floor(progress.loaded / progress.total * 100)
+
+      console.log('Upload progress:', percent + '%')
+
+      if(percent < 100 || this.uploadProgressCounter > 0){
+        this.progressMeterBar.css('width', percent + '%')
+        this.widget.removeClass('unknown')
+      }
     }
-  })
-}
 
-function warnRecordingSaveError () {
-  alert('There was an error saving your recording to the server.  Please refresh this page and try again.')
-}
+    this.uploadProgressCounter++
+  }
 
-function warnUploadRecordingError () {
-  alert('There was an error uploading your file to the server.  Please refresh this page and try uploading the file again.')
+  this.updateProgressText = () => {
+    var formattedText
+    if(this.recordingInProgress){
+      var seconds = this.recordingTime
+      var hours = Math.floor(seconds / 3600)
+      seconds = seconds - (hours * 3600)
+      var minutes = Math.floor(seconds / 60)
+      seconds = seconds - (minutes * 60)
+      formattedText = minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0')
+      if(hours){
+        formattedText = hours + ':' + formattedText;
+      }
+    } else if(this.uploadInProgress){
+      formattedText = 'UPLOADING'
+    } else if(this.uploadSuccessDenouement){
+      formattedText = 'COMPLETE'
+    } else {
+      formattedText = '--:--'
+    }
+    this.progressText.text(formattedText)
+  }
+
+  this.startRecordingTime = () => {
+    this.updateProgressText()
+    this.recordingTimer = setInterval(() => {
+      this.recordingTime++
+      this.updateProgressText()
+    }, 1000)
+  }
+
+  this.clearRecordingTime = () => {
+    clearInterval(this.recordingTimer)
+    this.recordingTime = 0
+    this.updateProgressText()
+  }
+
+  this.warnRecordingSaveError = () => {
+    alert('There was an error saving your recording to the server.  Please refresh this page and try again.')
+  }
+
+  this.warnUploadRecordingError = () => {
+    alert('There was an error uploading your file to the server.  Please refresh this page and try uploading the file again.')
+  }
+
+  this.deleteRecording = (id) => {
+    if (confirm('Are you sure you want to delete the recording?') == true) {
+      $.ajax({
+        url: '/organizer/recording/delete/' + id,
+        method: 'DELETE',
+        beforeSend: (jqXHR, settings) => {
+          jqXHR.setRequestHeader('X-CSRF-TOKEN', $('meta[name="_token"]').attr('content'))
+        },
+        success: (result) => {
+          location.reload()
+        }
+      })
+    }
+  }
 }
 
 
 $(document).ready(function () {
-  window.onbeforeunload = function () {
-    if (recordingsInProgress > 0) {
-      return 'Recording in progress. Navigating away from the page will lose your recording. Are you sure you want to continue?'
-    }
-    if (uploadsInProgress > 0) {
-      return 'Upload in progress. Navigating away from the page will lose your file. Are you sure you want to continue?'
-    }
-  }
+  // Initialize all audio recorder widgets.
+  $('.audio-recorder').each(function(i, element){
+    window.audioRecorers.push(new AudioRecorder(element))
+  })
+
+  // Initialize the Dropzone.
   // eslint-disable-next-line no-undef
   Dropzone.autoDiscover = false
   $('#myAwesomeDropzone').dropzone({
--- resources/views/scores/choirs_judge_aggregate.blade.php
+++ resources/views/scores/choirs_judge_aggregate.blade.php
@@ -44,13 +44,23 @@
     <td>
 
       @if($round->is_scoring_active == true && $judge_id == Auth::user()->person_id && $competition->organization->is_premium == 1)
-      <div id="controls">
-        <button id="{{'recordButton-'.$choir->id }}" data-count="{{(count($choir->recordings) > 0)?$choir->recordings->first()->total:'0'}}" data-recording="0" class="rbutton" data-choir="{{$choir->id}}" data-round="{{$round->id}}" data-division="{{$round->division_id}}">Start Recording({{(count($choir->recordings) > 0)?$choir->recordings->first()->total:'0'}})</button>
-        <div class="slider" id="{{'sliderId-'.$choir->id }}" style="display:none">
-              <div class="line"></div>
-              <div class="subline inc"></div>
-              <div class="subline dec"></div>
-      </div>
+      @php $recording_count = (count($choir->recordings) > 0)? $choir->recordings->first()->total : 0; @endphp
+      <div class="audio-recorder" id="audio-recorder-{{ $choir->id }}" data-count="{{ $recording_count }}" data-choir="{{ $choir->id }}" data-round="{{ $round->id }}" data-division="{{ $round->division_id }}">
+        <div class="ar-control">
+          <button>
+            <span class="ar-control-symbol"></span>
+          </button>
+        </div>
+        <div class="ar-info">
+          <div class="ar-title">Audio Recorder</div>
+          <div class="ar-existing">{{ $recording_count }} {{ $recording_count == 1 ? 'Recording' : 'Recordings' }} on File</div>
+        </div>
+        <div class="ar-progress">
+          <div class="ar-meter-box">
+            <div class="ar-meter-bar"></div>
+          </div>
+          <div class="ar-progress-text">--:--</div>
+        </div>
       </div>
 
 
@@ -73,19 +83,238 @@
 
 @section('body-footer')
 <script>
-  $(document).ready(function(){
-    $('.rbutton').click(function(e){
-      var choir = $(e.target).data('choir');
-      var round = $(e.target).data('round');
-      var division = $(e.target).data('division');
-      startRecording(choir, round, division);
-    });
-  });
 </script>
 @endsection
 
 @section('style')
 <style lang="scss">
+.audio-recorder {
+  position: relative;
+  min-width: 240px;
+  height: 75px;
+  line-height: 1;
+  color: #ffffff;
+  background: #4b5256;
+}
+
+.ar-info {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 75px;
+  padding: 8px 4px 0 12px;
+  text-align: left;
+}
+
+.ar-title {
+  font-weight: bold;
+  font-size: 16px;
+  margin-bottom: 8px;
+}
+
+.ar-existing {
+  font-size: 14px;
+  color: #e5e5e5;
+  margin-bottom: 5px;
+  /*text-decoration: underline;
+  cursor: pointer;*/
+}
+
+.ar-control {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 75px;
+  height: 75px;
+  border-right: 1px #808080 solid;
+}
+
+.ar-control button {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 0;
+  background: #903d9a;
+  border: none;
+  transition: all 0.25s ease;
+}
+
+.ar-control button:hover {
+  background: #7F4091;
+}
+
+.ar-control button::after {
+  content: 'RECORD';
+  display: block;
+  position: absolute;
+  bottom: 9px;
+  left: 0;
+  right: 0;
+  font-size: 11px;
+  text-transform: uppercase;
+}
+
+.recording .ar-control button {
+  background: #ee191c;
+}
+
+.recording .ar-control button:hover {
+  background: #de2131;
+}
+
+.recording .ar-control button::after {
+  content: 'STOP';
+}
+
+.disabled .ar-control button {
+  background: transparent;
+  cursor: not-allowed;
+}
+
+.disabled .ar-control button::after {
+  opacity: .5;
+}
+
+.ar-control-symbol {
+  display: block;
+  position: absolute;
+  top: 34px;
+  left: 50%;
+  margin: -10px;
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  background: #ffffff;
+  transition: all 0.5s ease;
+}
+
+.recording .ar-control-symbol {
+  border-radius: 0;
+}
+
+.disabled .ar-control-symbol {
+  opacity: .5;
+}
+
+.ar-progress {
+  position: absolute;
+  bottom: 0;
+  left: 75px;
+  right: 0;
+  height: 16px;
+  background: #272B2D;
+  box-sizing: content-box;
+  border-top: 1px #808080 solid;
+}
+
+.ar-progress .ar-meter-box {
+  position: absolute;
+  top: 5px;
+  right: 44px;
+  bottom: 5px;
+  left: 5px;
+  border: 1px #606060 solid;
+  border-radius: 3px;
+}
+
+.uploading .ar-progress .ar-meter-box {
+  right: 74px;
+}
+
+.ar-progress .ar-meter-box .ar-meter-bar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 100%;
+  background: #ee191c;
+  border-radius: 3px;
+  transition: all 0.5s ease;
+}
+
+.ar-progress .ar-meter-box .ar-meter-bar::after {
+  content: ' ';
+  display: block;
+  position: absolute;
+  top: 3px;
+  right: 0;
+  width: 0;
+  height: 0;
+  border-radius: 5px;
+  background: #ee191c;
+  border: 0 rgba(255, 127, 127, 0) solid;
+}
+
+.recording .ar-progress .ar-meter-box .ar-meter-bar {
+  width: 85%;
+}
+
+.recording .ar-progress .ar-meter-box .ar-meter-bar::after {
+  top: -3px;
+  right: -5px;
+  width: 10px;
+  height: 10px;
+  border: 1px rgba(255, 127, 127, 1) solid;
+  animation: glow 1s alternate ease infinite;
+}
+
+@keyframes glow {
+  from {
+    box-shadow: 0 0 10px 0 rgba(255, 0, 0, 0);
+  }
+  to {
+    box-shadow: 0 0 10px 2px rgba(255, 0, 0, 1);
+  }
+}
+
+.uploading .ar-progress .ar-meter-box .ar-meter-bar {
+  background: #0985eb;
+}
+
+.uploading.unknown .ar-progress .ar-meter-box .ar-meter-bar {
+  width: 100%;
+  background: #0985eb;
+  background-image: linear-gradient(
+    -45deg,
+    #0985eb 25%,
+    #80c6ff 25%,
+    #80c6ff 50%,
+    #0985eb 50%,
+    #0985eb 75%,
+    #80c6ff 75%,
+    #80c6ff
+  );
+  background-size: 50px 50px;
+  animation: move 2s linear infinite;
+}
+
+@keyframes move {
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: 50px 50px;
+  }
+}
+
+.ar-progress .ar-progress-text {
+  position: absolute;
+  top: 2px;
+  right: 2px;
+  width: 38px;
+  font-size: 11px;
+  text-align: center;
+}
+
+.uploading .ar-progress .ar-progress-text {
+  width: 68px;
+}
+
+
+/*
 button,
 .rbutton {
   background: #7f4091;
@@ -97,9 +326,10 @@ button,
   border-radius: 5px;
 }
 .cancel {
-    background-color: #cccccc;
-    color: #666666;
-    padding: 9px 14px;
-  }
+  background-color: #cccccc;
+  color: #666666;
+  padding: 9px 14px;
+ }
+*/
 </style>
 @endsection
