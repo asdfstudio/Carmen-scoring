f896a99d7947ea3b2f702e2a99bfaff4fb072f4a
Reed Hewitt <reed@reedhewitt.com>
2020-03-11 22:10:00 +0000

    Remove debug line.

--- resources/views/recordings/list.blade.php
+++ resources/views/recordings/list.blade.php
@@ -32,7 +32,6 @@
       @php
         $recordings = $choir->recordings->where('judge_id', $judge_id)->where('round_id', $round_id);
         $recording_count = $recordings->count();
-        dd($judgeList);
       @endphp
       <h4>Recorded Audio Comments by {{ $judgeList[$judge_id] }}</h4>
       <div class="audio-recorder tall-playlist" id="audio-recorder-{{ $choir->id }}" data-mode="player" data-count="{{ $recording_count }}" data-choir="{{ $choir->id }}" data-round="{{ $round->id }}" data-division="{{ $round->division_id }}">
