5742db42872c7739b29eb904b44ef0f3b0fd996f
Reed Hewitt <reed@reedhewitt.com>
2020-03-11 22:08:57 +0000

    Fix issue if judge ID is not yet set.

--- resources/views/recordings/list.blade.php
+++ resources/views/recordings/list.blade.php
@@ -27,56 +27,56 @@
       {{ Form::hidden('round_id', $round_id) }}
       {{ Form::hidden('judge_id', $judge_id) }}
       {!! Form::close() !!}
-  @endif
 
-  <div class="recording-wrapper">
-    @php
-      $recordings = $choir->recordings->where('judge_id', $judge_id)->where('round_id', $round_id);
-      $recording_count = $recordings->count();
-    @endphp
-    <h4>Recorded Audio Comments by {{ $judgeList[$judge_id] }}</h4>
-    <div class="audio-recorder tall-playlist" id="audio-recorder-{{ $choir->id }}" data-mode="player" data-count="{{ $recording_count }}" data-choir="{{ $choir->id }}" data-round="{{ $round->id }}" data-division="{{ $round->division_id }}">
-      <div class="ar-control">
-        <button>
-          <span class="ar-control-symbol"></span>
-        </button>
-      </div>
-      <div class="ar-info">
-        <div class="ar-title">Audio Player</div>
-        <div class="ar-title-playback">Playing: <span></span></div>
-        <div class="ar-existing">{{ $recording_count }} {{ $recording_count == 1 ? 'Recording' : 'Recordings' }} on File</div>
-      </div>
-      <div class="ar-playback-close"></div>
-      <div class="ar-progress">
-        <div class="ar-meter-box">
-          <div class="ar-meter-bar"></div>
+    <div class="recording-wrapper">
+      @php
+        $recordings = $choir->recordings->where('judge_id', $judge_id)->where('round_id', $round_id);
+        $recording_count = $recordings->count();
+        dd($judgeList);
+      @endphp
+      <h4>Recorded Audio Comments by {{ $judgeList[$judge_id] }}</h4>
+      <div class="audio-recorder tall-playlist" id="audio-recorder-{{ $choir->id }}" data-mode="player" data-count="{{ $recording_count }}" data-choir="{{ $choir->id }}" data-round="{{ $round->id }}" data-division="{{ $round->division_id }}">
+        <div class="ar-control">
+          <button>
+            <span class="ar-control-symbol"></span>
+          </button>
         </div>
-        <div class="ar-progress-text">--:--</div>
-      </div>
-      <div class="ar-playlist">
-        @if($recording_count === 0)
-          <ol class="empty">
-            <li>No recordings on file.</li>
-          </ol>
-        @else
-          <ol>
-            @foreach($recordings as $recording)
-              <li id="recording-{{ $recording->id }}" data-id="{{ $recording->id }}" data-url="{{ $recording->url }}">
-                <audio><source src="{{ $recording->url }}"></audio>
-                <span class="recording-name">{{ date('M. j, Y \a\t h:m:i A (\U\T\C)', strtotime($recording->created_at)) }}</span>
-                <div class="ar-playlist-functions">
-                  <button class="ar-playlist-play-pause" title="Play/Pause"></button>
-                  <a class="ar-playlist-download" title="Download Recording" href="{{ $recording->url }}" target="_blank" download="{{ date('M. j, Y \a\t h:m:i A (\U\T\C)', strtotime($recording->created_at)) }}" type="application/octet-stream"></a>
-                  @can('create', $division)
-                    <button class="ar-playlist-delete" title="Delete Recording"></button>
-                  @endcan
-                </div>
-              </li>
-            @endforeach
-          </ol>
-        @endif
+        <div class="ar-info">
+          <div class="ar-title">Audio Player</div>
+          <div class="ar-title-playback">Playing: <span></span></div>
+          <div class="ar-existing">{{ $recording_count }} {{ $recording_count == 1 ? 'Recording' : 'Recordings' }} on File</div>
+        </div>
+        <div class="ar-playback-close"></div>
+        <div class="ar-progress">
+          <div class="ar-meter-box">
+            <div class="ar-meter-bar"></div>
+          </div>
+          <div class="ar-progress-text">--:--</div>
+        </div>
+        <div class="ar-playlist">
+          @if($recording_count === 0)
+            <ol class="empty">
+              <li>No recordings on file.</li>
+            </ol>
+          @else
+            <ol>
+              @foreach($recordings as $recording)
+                <li id="recording-{{ $recording->id }}" data-id="{{ $recording->id }}" data-url="{{ $recording->url }}">
+                  <audio><source src="{{ $recording->url }}"></audio>
+                  <span class="recording-name">{{ date('M. j, Y \a\t h:m:i A (\U\T\C)', strtotime($recording->created_at)) }}</span>
+                  <div class="ar-playlist-functions">
+                    <button class="ar-playlist-play-pause" title="Play/Pause"></button>
+                    <a class="ar-playlist-download" title="Download Recording" href="{{ $recording->url }}" target="_blank" download="{{ date('M. j, Y \a\t h:m:i A (\U\T\C)', strtotime($recording->created_at)) }}" type="application/octet-stream"></a>
+                    @can('create', $division)
+                      <button class="ar-playlist-delete" title="Delete Recording"></button>
+                    @endcan
+                  </div>
+                </li>
+              @endforeach
+            </ol>
+          @endif
+        </div>
       </div>
     </div>
-  </div>
-
+  @endif
 @endif
