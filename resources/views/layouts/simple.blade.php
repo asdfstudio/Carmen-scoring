8eeaa00552b1566a9c970d4ac5fadfb6054b39b5
Andrew Jennings <afjennings@gmail.com>
2020-08-11 16:20:29 -0400

    Replace light-text logo with dark-text logo

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -11,7 +11,7 @@
   <div class="competition-bar-wrap">
     <div class="competition-bar body-width">
       <div class="carmen-logo-wrap">
-        <img src="/images/logo-with-text.png" width="185" />
+        <img src="/images/logo-with-dark-text.png" width="185" />
       </div>
       <div class="heading">
         {{ link_to_route(Request::segment(1) . '.competition.show', $competition->name, [$competition]) }}
