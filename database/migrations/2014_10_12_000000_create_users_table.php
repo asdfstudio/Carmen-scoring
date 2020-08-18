d7da349495d7e5f6d65d0bf9a5667c1c39434374
Andrew Jennings <afjennings@gmail.com>
2020-08-18 16:56:46 -0400

    Pull in some new files from Laravel 7
    Update to latest artisan file, .gitignore, database migrations,
    autoloader, testing, CORS, web config, etc.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
@@ -13,8 +14,8 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            //$table->string('name');
+            $table->id();
+            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
@@ -32,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('users');
     }
 }
