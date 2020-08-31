b8528c7085ff382cd69f91eedb6a94d3251110f4
unknown <upwork@cwdgservices.com>
2020-08-31 14:55:56 -0500

    Create/Edit division, fix link issue, fix lable binding

--- resources/views/vendor/laravel-form-builder/radio.php
+++ resources/views/vendor/laravel-form-builder/radio.php
@@ -7,16 +7,11 @@
 <?php if ($showField): ?>
     <?= Form::radio($name, $options['value'], $options['checked'], $options['attr']) ?>
 
-    <?php include 'help_block.php' ?>
-<?php endif; ?>
-
-<?php if ($showLabel && $options['label'] !== false && $options['label_show']): ?>
-    <?php if ($options['is_child']): ?>
-        <?= Form::label($name, $options['label'], $options['label_attr']) ?>
-        <!--<label <?//= $options['labelAttrs'] ?>><?//= $options['label'] ?></label>-->
-    <?php else: ?>
-        <?= Form::label($name, $options['label'], $options['label_attr']) ?>
+    <?php if ($showLabel && $options['label'] !== false && $options['label_show']): ?>
+        <?= Form::customLabel($name, $options['label'], $options['label_attr']) ?>
     <?php endif; ?>
+
+    <?php include 'help_block.php' ?>
 <?php endif; ?>
 
 <?php include 'errors.php' ?>
