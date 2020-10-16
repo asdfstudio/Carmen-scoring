004e023ab0ef62a7328b57bfb0604a03ae524b7a
unknown <seniorjamal5@gmail.com>
2020-10-17 02:58:44 +0800

    Fix commas

--- database/factories/RoundFactory.php
+++ database/factories/RoundFactory.php
@@ -8,11 +8,11 @@ use Faker\Generator as Faker;
 $factory->define(Round::class, function (Faker $faker) {
 
     return [
-      'name' => $faker->word
-    , 'competition_id' => factory(App\Competition::class)
-    , 'caption_weighting_id' => factory(App\CaptionWeighting::class)
-    , 'scoring_method_id' => factory(App\ScoringMethod::class)
-    , 'sheet_id' => factory(App\Sheet::class)
-    , 'sequence' => $faker->numberBetween(0,1)
+      'name' => $faker->word,
+      'competition_id' => factory(App\Competition::class),
+      'caption_weighting_id' => factory(App\CaptionWeighting::class),
+      'scoring_method_id' => factory(App\ScoringMethod::class),
+      'sheet_id' => factory(App\Sheet::class),
+      'sequence' => $faker->numberBetween(0,1)
     ];
 });
