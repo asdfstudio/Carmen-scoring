b46a0d101c38354ed4ddacc17c4dd90893ecfd27
unknown <seniorjamal5@gmail.com>
2020-10-17 02:32:29 +0800

    Fix seeders

--- database/seeds/CommentSeeder.php
+++ database/seeds/CommentSeeder.php
@@ -15,7 +15,7 @@ class CommentSeeder extends Seeder
   {
     // for each judge in the division, add a comment for each choir
     App\Division::all()->each(function($division) {
-      $round = $division->round();
+      $round = $division->round()->first();
       $judges = $division->judges;
       $choirs = $division->choirs;
       foreach ($judges as $judge) {
--- database/seeds/RawScoreSeeder.php
+++ database/seeds/RawScoreSeeder.php
@@ -13,7 +13,7 @@ class RawScoreSeeder extends Seeder
   {
     $competition = App\Competition::firstWhere('name', 'Demo Competition');
     foreach ($competition->divisions as $division) {
-      $round = $division->round();
+      $round = $division->round()->first();
       $judges = $division->judges;
       $choirs = $division->choirs;
       foreach ($choirs as $choir) {
