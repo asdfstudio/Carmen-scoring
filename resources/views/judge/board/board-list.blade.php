42ad5eff7d45a6290ef05d75ed7d61bbfb489c25
unknown <seniorjamal5@gmail.com>
2020-10-24 04:10:32 +0800

    Show change password button.

--- resources/views/judge/board/board-list.blade.php
+++ resources/views/judge/board/board-list.blade.php
@@ -7,8 +7,8 @@
   @can('importJudges', $round)
     <a class="add-resource" data-resource-type="judge" href="#">Add a Judge</a>
     <a class="import-resource" data-resource-type="judge" href="#">Import From Another Round</a>
-    <a class="change-password" href="#">Change Password</a>
   @endcan
+  <a class="change-password" href="#">Change Password</a>
   {!! form($newJudgeForm) !!}
 
   @php
