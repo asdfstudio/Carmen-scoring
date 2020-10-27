d1d0674cd67eb3eb34515a964379259444f36086
Andrew Jennings <afjennings@gmail.com>
2020-10-27 16:10:12 -0400

    Remove archived competitions from Judge list.

--- app/Http/Controllers/Judge/CompetitionController.php
+++ app/Http/Controllers/Judge/CompetitionController.php
@@ -14,46 +14,32 @@ use Auth;
 class CompetitionController extends Controller
 {
     public function index()
-		{
-			$judge_id = Auth::user()->person_id;
+    {
+        $judge_id = Auth::user()->person_id;
 
-			$competitions = Competition::withoutGlobalScope('organization')->whereHas('rounds.judges', function($query) use ($judge_id) {
-				$query->where('judge_id',$judge_id);
-			})->orWhereHas('soloDivisions.judges', function($query) use ($judge_id) {
-				$query->where('judge_id',$judge_id);
-			})->active()->get();
+        $competitions = Competition::withoutGlobalScope('organization')->whereHas('rounds.judges', function($query) use ($judge_id) {
+            $query->where('judge_id',$judge_id);
+        })->orWhereHas('soloDivisions.judges', function($query) use ($judge_id) {
+            $query->where('judge_id',$judge_id);
+        })->active()->get();
 
-			$archivedCompetitions = Competition::withoutGlobalScope('organization')->whereHas('rounds.judges',function($query) use ($judge_id) {
-				$query->where('judge_id',$judge_id);
-			})->orWhereHas('soloDivisions.judges', function($query) use ($judge_id) {
-				$query->where('judge_id',$judge_id);
-			})->archived()->get();
+        return view('competition.judge.index',compact('competitions'));
+    }
 
-			return view('competition.judge.index',compact('competitions','archivedCompetitions'));
-		}
+    public function show($id)
+    {
+        $judge_id = Auth::user()->person_id;
 
-		public function show($id)
-		{
-			$judge_id = Auth::user()->person_id;
+        $competition = Competition::withoutGlobalScope('organization')->with(['rounds' => function($query) use ($judge_id) {
+            $query->whereHas('judges', function($query) use ($judge_id) {
+                $query->where('judge_id', $judge_id);
+            });
+        }, 'soloDivisions' => function($query) use ($judge_id) {
+            $query->whereHas('judges', function($query) use ($judge_id) {
+                $query->where('judge_id', $judge_id);
+            });
+        },'rounds.judges'])->active()->find($id);
 
-			$competition = Competition::withoutGlobalScope('organization')->with(['rounds' => function($query) use ($judge_id) {
-					$query->whereHas('judges', function($query) use ($judge_id) {
-						$query->where('judge_id', $judge_id);
-					});
-				}, 'soloDivisions' => function($query) use ($judge_id) {
-  					$query->whereHas('judges', function($query) use ($judge_id) {
-  						$query->where('judge_id', $judge_id);
-  					});
-  				},'rounds.judges'])->active()->find($id);
-
-      //dd($competition);
-
-			/*$competition = Competition::with(['divisions','divisions.judges'])->active()->find($id);
-
-			$filtered = $competition->divisions->filter(function($division,$key) {
-				return $value->judges
-			});*/
-
-			return view('competition.judge.show',compact('competition'));
-		}
+        return view('competition.judge.show',compact('competition'));
+    }
 }
--- resources/views/competition/judge/index.blade.php
+++ resources/views/competition/judge/index.blade.php
@@ -2,14 +2,8 @@
 
 @section('content')
 
-
-
   <h1>Active and Upcoming Competitions</h1>
 
   @include('competition.judge.list')
-
-
-  <h2>Archived Competitions</h2>
-  @include('competition.judge.list',['competitions' => $archivedCompetitions])
 
 @endsection
