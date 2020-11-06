6fb70a3e7a3da8ec5e26cc4f82684c1f80e5ff34
Andrew Jennings <afjennings@gmail.com>
2020-11-06 13:06:10 -0500

    Fix issue with multiple confirmAndSubmit buttons in Division Forms.

--- app/Forms/Scoring/CompleteScoringForm.php
+++ app/Forms/Scoring/CompleteScoringForm.php
@@ -20,7 +20,8 @@ class CompleteScoringForm extends Form
           $btnAttr = ['class' => 'action'];
         }
 
-        $btnAttr['onclick'] = 'confirmAndSubmit("You are about to lock in scores for this division. This action prevents the judges from making any changes and it calculates the results. This action can be undone by clicking ‘activate scoring’.", "complete_scoring_form");';
+        $btnAttr['onclick'] = 'confirmAndSubmit("You are about to lock in scores for this division. This action prevents the judges from making any changes and it calculates the results. '.
+            'This action can be undone by clicking ‘activate scoring’.", null, this);';
 
         $this->add('complete','hidden',['value' => '1']);
         $this->add('button', 'button', [
--- app/Forms/Scoring/FinalizeScoringForm.php
+++ app/Forms/Scoring/FinalizeScoringForm.php
@@ -17,10 +17,11 @@ class FinalizeScoringForm extends Form
     {
         $this->add('finalize','hidden',['value' => '1']);
         $this->add('button', 'button', [
-          'label' => 'Send scores and feedback', 
+          'label' => 'Send scores and feedback',
           'attr' => [
             'class' => $this->formOptions['disabled'] ? 'action disabled' : 'action',
-            'onclick' => 'confirmAndSubmit("You are about to send scores and feedback to all participating directors in this division. This action can be undone by clicking ‘activate scoring’. If you do activate scoring again, the directors will be blocked from viewing all scores and feedback until the ‘send scores and feedback’ button is clicked again.", "finalize_scoring_form")'
+            'onclick' => 'confirmAndSubmit("You are about to send scores and feedback to all participating directors in this division. This action can be undone by clicking ‘activate scoring’. '.
+                'If you do activate scoring again, the directors will be blocked from viewing all scores and feedback until the ‘send scores and feedback’ button is clicked again.", null, this)'
           ]
         ]);
     }
