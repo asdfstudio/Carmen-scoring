e97fce1ba6223db1149c52b02cf03002620e3ffa
Andrew Jennings <afjennings@gmail.com>
2020-11-13 12:13:09 -0500

    Restore mistakenly removed use statement.

--- app/Http/Controllers/Judge/SoloDivisionPerformerController.php
+++ app/Http/Controllers/Judge/SoloDivisionPerformerController.php
@@ -13,6 +13,7 @@ use App\SoloRawScore;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Events\CommentSaved;
+use App\Carmen\SoloScorekeeper;
 use App\Http\Controllers\Controller;
 use Kris\LaravelFormBuilder\FormBuilder;
 
