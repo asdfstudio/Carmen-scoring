de7a718e3d96808a5f5d0022c7616a2adb2c3d04
Andrew Jennings <afjennings@gmail.com>
2021-01-26 20:27:45 -0500

    Fix copyright warning on audience vote page.

--- resources/views/votes/partial/footer.blade.php
+++ resources/views/votes/partial/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6 col-md-8 col-12">
-          Copy right © Voting {{date('Y')}}. All Right Reserved.
+          © {{date('Y')}} Carmen Scoring
         </div>
       </div>
     </div>
