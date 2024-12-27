feb9908654979da7846bba03d86653eaf0827da8
TIRony <rony120114@gmail.com>
2024-12-27 22:10:09 +0600

    Completed CD-204 fixing

--- resources/views/competition/organizer/show.blade.php
+++ resources/views/competition/organizer/show.blade.php
@@ -59,75 +59,76 @@
               {{ link_to_route('organizer.competition.division.index','Manage your classes',[$competition], ['class' => 'action']) }}
           </p>
       </div>
-      <div class="action-buttons" style="display: flex; gap: 2px;">
-      @if(isset($activateAllScoringForm))
-          @php
-              // Check if all divisions have scoring activated or completed
-              $canActivateAllScoring = false;
-              foreach ($competition->divisions as $division) {
-                  if ($division->canActivateScoring()) {
-                      $canActivateAllScoring = true;
-                      break; // No need to check further if one can be activated
-                  }
-              }
-          @endphp
+      @if($divisionCount > 0)
+        <div class="action-buttons" style="display: flex; gap: 2px;">
+            @if(isset($activateAllScoringForm))
+                @php
+                    $canActivateAllScoring = false;
+                    foreach ($competition->divisions as $division) {
+                        if ($division->canActivateScoring()) {
+                            $canActivateAllScoring = true;
+                            break;
+                        }
+                    }
+                @endphp
 
-          {!! form($activateAllScoringForm, [
-              'attr' => [
-                  'disabled' => !$canActivateAllScoring ? 'disabled' : null,
-                  'style' => !$canActivateAllScoring ? 'cursor: not-allowed; opacity: 0.5;' : ''
-              ]
-          ]) !!}
+                {!! form($activateAllScoringForm, [
+                    'attr' => [
+                        'disabled' => !$canActivateAllScoring ? 'disabled' : null,
+                        'style' => !$canActivateAllScoring ? 'cursor: not-allowed; opacity: 0.5;' : ''
+                    ]
+                ]) !!}
+            @endif
+
+
+            @if(isset($completeAllScoringForm))
+                @php
+                    $canCompleteAllScoring = true;
+                    foreach ($competition->divisions as $division) {
+                        if (!$division->canCompleteScoring()) {
+                            $canCompleteAllScoring = false;
+                            break;
+                        }
+                    }
+                @endphp
+                {!! form($completeAllScoringForm, [
+                    'attr' => [
+                        'disabled' => !$canCompleteAllScoring ? 'disabled' : null,
+                        'data-can-complete' => $canCompleteAllScoring ? 'true' : 'false',
+                        'style' => !$canCompleteAllScoring ? 'cursor: not-allowed !important; opacity: 0.5;' : ''
+                    ]
+                ]) !!}
+            @endif
+
+            <script>
+                document.addEventListener('DOMContentLoaded', function () {
+                    const completeAllScoringButton = document.querySelector('[data-can-complete="false"]');
+                    if (completeAllScoringButton) {
+                        completeAllScoringButton.querySelector('.action').disabled = true;
+                    }
+                });
+          </script>
+
+            @if(isset($sendAllScoresAndFeedbackForm))
+                @php
+                    $canSendAllScores = true;
+                    foreach ($competition->divisions as $division) {
+                        if (!$division->is_completed) {
+                            $canSendAllScores = false;
+                            break;
+                        }
+                    }
+                @endphp
+                {!! form($sendAllScoresAndFeedbackForm, [
+                    'attr' => [
+                        'disabled' => !$canSendAllScores ? 'disabled' : null,
+                        'data-can-complete' => $canSendAllScores ? 'true' : 'false',
+                        'style' => !$canSendAllScores ? 'cursor: not-allowed !important; opacity: 0.5;' : ''
+                    ]
+                ]) !!}
+            @endif
+        </div>
       @endif
-
-
-          @if(isset($completeAllScoringForm))
-              @php
-                  $canCompleteAllScoring = true;
-                  foreach ($competition->divisions as $division) {
-                      if (!$division->canCompleteScoring()) {
-                          $canCompleteAllScoring = false;
-                          break;
-                      }
-                  }
-              @endphp
-              {!! form($completeAllScoringForm, [
-                  'attr' => [
-                      'disabled' => !$canCompleteAllScoring ? 'disabled' : null,
-                      'data-can-complete' => $canCompleteAllScoring ? 'true' : 'false',
-                      'style' => !$canCompleteAllScoring ? 'cursor: not-allowed; opacity: 0.5;' : ''
-                  ]
-              ]) !!}
-          @endif
-
-          <script>
-              document.addEventListener('DOMContentLoaded', function () {
-                  const completeAllScoringButton = document.querySelector('[data-can-complete="false"]');
-                  if (completeAllScoringButton) {
-                      completeAllScoringButton.querySelector('.action').disabled = true;
-                  }
-              });
-        </script>
-
-          @if(isset($sendAllScoresAndFeedbackForm))
-              @php
-                  $canSendAllScores = true;
-                  foreach ($competition->divisions as $division) {
-                      if (!$division->is_completed) {
-                          $canSendAllScores = false;
-                          break;
-                      }
-                  }
-              @endphp
-              {!! form($sendAllScoresAndFeedbackForm, [
-                  'attr' => [
-                      'disabled' => !$canSendAllScores ? 'disabled' : null,
-                      'data-can-complete' => $canSendAllScores ? 'true' : 'false',
-                      'style' => !$canSendAllScores ? 'cursor: not-allowed; opacity: 0.5;' : ''
-                  ]
-              ]) !!}
-          @endif
-      </div>
     </div>
 
       @include('round.organizer.list',['rounds' => $competition->rounds])
